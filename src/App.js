8f04eeaa625c41a391a964ea3dc08a0080249dfd
Khaled-Atef <khaledatef961@gmail.com>
2021-09-15 19:50:07 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,38 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <form className="needs-validation">
+    <div className="form-group">
+    <label htmlFor="uname">First Name:</label>
+      <input type="text" className="form-control" id="uname" placeholder="Enter First Name" name="uname" required/>
+    <label htmlFor="uname">Last Name:</label>
+      <input type="text" className="form-control" id="uname" placeholder="Enter Last Name" name="uname" required/>
+    <label htmlFor="uname">Gender:</label>
+    <div className="form-check">
+      <label className="form-check-label" htmlFor="check1">
+        <input type="checkbox" className="form-check-input" id="check1" name="option1" value="something" />Male
+      </label>
+    </div>
+    <div className="form-check">
+      <label className="form-check-label" htmlFor="check2">
+        <input type="checkbox" className="form-check-input" id="check2" name="option2" value="something"/>Female
+      </label>
+    </div>
+    <br/>
+    <label htmlFor="email">Email:</label>
+      <input type="email" className="form-control" id="email" placeholder="Enter email" name="email"/>
+    </div>
+    <div className="form-group">
+    <label htmlFor="pwd">Password:</label>
+      <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pswd"/>
+    <label htmlFor="pwd">confirm Password:</label>
+      <input type="password" className="form-control" id="pwd" placeholder="confirm password" name="pswd"/>
+    </div>
+    
+    
+    <button type="submit" className="btn btn-primary">Sign in</button>
+  </form>
     </div>
   );
 }
